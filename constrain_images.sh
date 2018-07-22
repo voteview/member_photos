4a3153bae6048582eef5f2af107814ccc2af819f
aaronrudkin <junk_signups@rudkin.ca>
2018-07-22 02:07:28 -0700

    Images as of July 22, 2018

--- constrain_images.sh
+++ constrain_images.sh
@@ -2,3 +2,12 @@
 mogrify -verbose -format jpg -quality 75 -resize "2000x2000>" -path images/bio_guide images/raw/bio_guide/*.*
 mogrify -verbose -format jpg -quality 75 -resize "2000x2000>" -path images/wiki images/raw/wiki/*.*
 mogrify -verbose -format jpg -quality 75 -resize "2000x2000>" -path images/manual images/raw/manual/*.*
+
+jpegoptim --strip-all -P images/manual/*
+jpegoptim --strip-all -P images/wiki/*
+jpegoptim --strip-all -P images/bio_guide/*
+
+find images/manual -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
+find images/wiki -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
+find images/bio_guide -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
+
