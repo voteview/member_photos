7acab7459539bb4d7faaa8942c6d7280f4449bce
aaronrudkin <junk_signups@rudkin.ca>
2018-07-21 18:41:38 -0700

    Moved manual images to raw directory, going to re-derive them.

--- constrain_images.sh
+++ constrain_images.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mogrify -verbose -format jpg -quality 75 -resize "2000x2000>" -path images/bio_guide images/raw/bio_guide/*.*
 mogrify -verbose -format jpg -quality 75 -resize "2000x2000>" -path images/wiki images/raw/wiki/*.*
+mogrify -verbose -format jpg -quality 75 -resize "2000x2000>" -path images/manual images/raw/manual/*.*
