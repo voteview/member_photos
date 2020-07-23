1843082c14522146ca8338a394c65f4ea819ae6d
Aaron Rudkin <junk_signups@rudkin.ca>
2020-07-23 01:13:28 -0700

    Fix provenance for Pete du Pont and cleaned up error output of verify.py

--- verify.py
+++ verify.py
@@ -84,41 +84,43 @@ def verify(do_flush):
                "Remove manual or wiki sources in favor of official sources."))
         print(multiple_set)
 
-    if any([number_missing_current > 1,
-            diff_set,
-            photos_missing,
-            unknown_provenance]):
-        print("We have one or more data integrity issues.")
+    if not any([number_missing_current > 1, diff_set, photos_missing,
+                unknown_provenance]):
+        return
 
-        if number_missing_current:
-            print("Missing %d images for modern "
-                  "members." % number_missing_current)
-        if diff_set:
-            print("Some images we possess are not represented in members file.")
-            print(diff_set)
-            if do_flush:
-                for i in diff_set:
-                    if i.rsplit("/", 1)[1] in multiple_set:
-                        os.unlink(i)
-                        i_out = (i.replace("images/", "images/raw/")
-                                 .replace(".jpg", ".*"))
-                        for file in glob.glob(i_out):
-                            os.unlink(file)
-                    else:
-                        print(("%s not included in members file, but not a "
-                               "duplicate either. Regenerate members file "
-                               "with `config/dump_csv.py`?" % i))
+    print("We have one or more data integrity issues.")
 
-                print("Flushed those files.")
+    if number_missing_current:
+        print("Missing %d images for modern "
+                "members." % number_missing_current)
 
-        if photos_missing:
-            print("Some images in the members file are not in our possession.")
-            print(list(photos_missing)[0:10])
-        if len(unknown_provenance) > 4:
-            print("Some members have no provenance statement for their image.")
-            print(unknown_provenance)
+    if diff_set:
+        print("Some images we possess are not represented in members file.")
+        print(diff_set)
+        if do_flush:
+            for i in diff_set:
+                if i.rsplit("/", 1)[1] in multiple_set:
+                    os.unlink(i)
+                    i_out = (i.replace("images/", "images/raw/")
+                                .replace(".jpg", ".*"))
+                    for file in glob.glob(i_out):
+                        os.unlink(file)
+                else:
+                    print(("%s not included in members file, but not a "
+                            "duplicate either. Regenerate members file "
+                            "with `config/dump_csv.py`?" % i))
 
-        sys.exit(1)
+            print("Flushed those files.")
+
+    if photos_missing:
+        print("Some images in the members file are not in our possession.")
+        print(list(photos_missing)[0:10])
+
+    if unknown_provenance:
+        print("Some members have no provenance statement for their image.")
+        print(unknown_provenance)
+
+    sys.exit(1)
 
 def parse_arguments():
     """ Parse command line arguments and launch the process. """
