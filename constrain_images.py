13d63bc4a9569dc8c0b87b23278da0ddd94088c9
Aaron Rudkin <junk_signups@rudkin.ca>
2021-01-23 11:12:38 +0000

    Miscellaneous linting cleanup

--- constrain_images.py
+++ constrain_images.py
@@ -1,3 +1,9 @@
+"""
+Crops and resizes images dynamically. Originally implemented as a shell
+script, but simpler to track relevant variables and keep track of which
+files need processing here.
+"""
+
 import glob
 import os
 import subprocess
@@ -5,36 +11,46 @@ import argparse
 from wand.image import Image
 
 def constrain_folder(folder, override):
+    """ Dispatches files in a folder to be constrained. """
     files = glob.glob("%s*.*" % folder)
     if override:
         files_edit = files
     else:
-        files_resized = ["%s.jpg" % x.replace("raw/", "").rsplit(".", 1)[0] for x in files]
-        files_edit = [x for x, y in zip(files, files_resized) if not os.path.isfile(y) or os.path.getmtime(y) < os.path.getmtime(x)]
+        files_resized = ["%s.jpg" % x.replace("raw/", "").rsplit(".", 1)[0]
+                         for x in files]
+        files_edit = [x for x, y in zip(files, files_resized)
+                      if not os.path.isfile(y) or
+                      os.path.getmtime(y) < os.path.getmtime(x)]
 
     for file_name in files_edit:
         constrain_image(file_name)
 
 def constrain_image(file_name):
+    """ Constrains an individual file. """
     print("Processing file %s" % file_name)
     with Image(filename=file_name) as img_in:
         aspect_ratio = float(img_in.size[0]) / float(img_in.size[1])
         width = img_in.size[0]
         height = img_in.size[1]
         new_folder = file_name.replace("raw/", "").rsplit("/", 1)[0]
-        new_filename = "%s.jpg" % file_name.replace("raw/", "").rsplit(".", 1)[0]
+        new_filename = ("%s.jpg" %
+                        file_name.replace("raw/", "").rsplit(".", 1)[0])
 
-        if aspect_ratio > 0.75 and aspect_ratio < 0.85 or "bio_guide" in file_name:
+        if (0.75 < aspect_ratio < 0.85 or "bio_guide" in file_name):
             print("\t Aspect ratio OK, resizing.")
-            args = ["mogrify", "-verbose", "-format jpg", "-quality 75", '-resize "600x600>"', "-path %s" % new_folder, file_name]
+            args = ["mogrify", "-verbose", "-format jpg", "-quality 75",
+                    '-resize "600x600>"', "-path %s" % new_folder, file_name]
         else:
             new_height = height if aspect_ratio > 1 else (width * 5/4)
             new_width = width if aspect_ratio < 1 else (height * 4/5)
             if new_height > 600:
                 new_height = 600
                 new_width = 480
-            print("\t Rescaling image from %s x %s (AR %s) to %s x %s" % (width, height, aspect_ratio, new_width, new_height))
-            args = ["smartcrop", "--width %s" % new_width, "--height %s" % new_height, "--faceDetection", "--outputFormat jpg", "--quality 80", file_name, new_filename]
+            print("\t Rescaling image from %s x %s (AR %s) to %s x %s" %
+                  (width, height, aspect_ratio, new_width, new_height))
+            args = ["smartcrop", "--width %s" % new_width, "--height %s" %
+                    new_height, "--faceDetection", "--outputFormat jpg",
+                    "--quality 80", file_name, new_filename]
 
         print(args)
         call = " ".join(args)
@@ -53,5 +69,4 @@ def parse_arguments():
     constrain_folder("images/raw/bio_guide/", override)
 
 if __name__ == "__main__":
-	parse_arguments()
-
+    parse_arguments()
--- resources/tile_grid.py
+++ resources/tile_grid.py
@@ -1,49 +1,60 @@
+""" Generates gridded walls of images from individual congress/chambers. """
+
 import csv
 import random
 from PIL import Image
 
 def load_scale(filename):
-	image = Image.open(filename)
-	original_size = image.size
-	new_width = 120
-	new_height = original_size[1] * (120 / float(original_size[0]))
-	image.thumbnail((new_width, new_height), Image.ANTIALIAS)
-	return image
+    """ Loads a single image and scales it to 120xheight """
+
+    image = Image.open(filename)
+    original_size = image.size
+    new_width = 120
+    new_height = original_size[1] * (120 / float(original_size[0]))
+    image.thumbnail((new_width, new_height), Image.ANTIALIAS)
+    return image
 
 def build_grid(filenames):
-	random.shuffle(filenames)
-	image = Image.new("RGB", (1100, 300))
-	x = 0 - random.randint(0, 60)
-	y = 0 - random.randint(0, 75)
-	for filename in filenames:
-		print(filename, x, y)
-		sprite = load_scale(filename)
-		image.paste(sprite, (x, y))
-		y = y + sprite.size[1] + 1
-		if y > 300:
-			x = x + 121
-			y = 0 - random.randint(0, 75)
-		if x > 1100:
-			break
+    """ Builds the actual grid images. """
 
-	return image
+    random.shuffle(filenames)
+    image = Image.new("RGB", (1100, 300))
+    x = 0 - random.randint(0, 60)
+    y = 0 - random.randint(0, 75)
+    for filename in filenames:
+        print(filename, x, y)
+        sprite = load_scale(filename)
+        image.paste(sprite, (x, y))
+        y = y + sprite.size[1] + 1
+        if y > 300:
+            x = x + 121
+            y = 0 - random.randint(0, 75)
+        if x > 1100:
+            break
+
+    return image
 
 def list_files(congress, chamber):
-	all_members = []
-	with open("../members.csv") as f:
-		reader = csv.DictReader(f)
-		for row in reader:
-			all_members.append(row)
+    """ List all images relevant to a given congress and chamber. """
 
-	return [x["image"].replace("images/", "../images/") for x in all_members if x["congress"] == str(congress) and x["chamber"] == chamber and x["image"]]
+    all_members = []
+    with open("../members.csv") as read_file:
+        reader = csv.DictReader(read_file)
+        for row in reader:
+            all_members.append(row)
+
+    return [x["image"].replace("images/", "../images/")
+            for x in all_members if x["congress"] == str(congress) and
+            x["chamber"] == chamber and x["image"]]
 
 def process(congress, chamber, filename_out):
-	filenames = list_files(congress, chamber)
-	print("Detected %s matching member portraits" % len(filenames))
-	if filenames:
-		grid = build_grid(filenames)
-		grid.save(filename_out, "JPEG")
+    """ Wraps the entire process. """
+    filenames = list_files(congress, chamber)
+    print("Detected %s matching member portraits" % len(filenames))
+    if filenames:
+        grid = build_grid(filenames)
+        grid.save(filename_out, "JPEG")
 
 if __name__ == "__main__":
-	process(117, "Senate", "senate_grid.jpg")
-	process(117, "House", "house_grid.jpg")
+    process(117, "Senate", "senate_grid.jpg")
+    process(117, "House", "house_grid.jpg")
