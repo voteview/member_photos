1ee3e38e1694b53ca3393cf060936962063190b0
aaronrudkin <junk_signups@rudkin.ca>
2021-01-28 04:44:15 -0800

    A bunch more aspect ratio fixes

--- verify.py
+++ verify.py
@@ -126,7 +126,7 @@ def check_aspect_ratio():
     results = []
 
     for image in bioguide_images:
-        if image.rsplit("/", 1)[1][0:3] not in ["021"]:
+        if image.rsplit("/", 1)[1][0:2] not in ["02"]:
             continue
         with Image(filename=image) as img_in:
             aspect_ratio = round(float(img_in.size[0]) / float(img_in.size[1]), 2)
