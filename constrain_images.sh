9e924ffe624abbef1d6e5d5e2692a0876bfa0b4b
aaronrudkin <junk_signups@rudkin.ca>
2021-01-28 04:19:33 -0800

    Improving constrain script for faster execution again. Fixed one more bad aspect ratio.

--- constrain_images.sh
+++ constrain_images.sh
@@ -6,14 +6,3 @@ rm images/raw/wiki/*.gif
 
 # Constrain all of the raw files that need constraint
 python constrain_images.py
-
-# Optimize
-jpegoptim --strip-all -P images/manual/*
-jpegoptim --strip-all -P images/wiki/*
-jpegoptim --strip-all -P images/bio_guide/*
-
-# JPEGTran
-find images/manual -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
-find images/wiki -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
-find images/bio_guide -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
-
