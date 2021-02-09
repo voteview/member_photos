dffb9622ca87856175bec47a5ce5504d7b0f815b
aaronrudkin <junk_signups@rudkin.ca>
2021-02-09 13:55:43 -0800

    Fixes some of the errors identified in 1st-65th congresses.

--- wiki.py
+++ wiki.py
@@ -749,6 +749,11 @@ def blacklist_icpsr(icpsr):
     new_blacklist = {str(x).zfill(6) for x in icpsr if x}
     existing_wiki["blacklist"] = list(blacklist | new_blacklist)
 
+    if os.path.isfile("images/wiki/%s.jpg" % str(icpsr).zfill(6)):
+        os.remove("images/wiki/%s.jpg" % str(icpsr).zfill(6))
+    if os.path.isfile("images/raw/wiki/%s.jpg" % str(icpsr).zfill(6)):
+        os.remove("images/raw/wiki/%s.jpg" % str(icpsr).zfill(6))
+
     # Write it out
     with open("config/wiki_results.json", "w") as out_file:
         json.dump(existing_wiki, out_file)
